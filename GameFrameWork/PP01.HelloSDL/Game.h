25031e629f8335460e7b4656b195a8971c90712b
user <user@532-14>
2018-11-13 12:27:50 +0900

    PP05.AnimatingSprite

--- GameFrameWork/PP01.HelloSDL/Game.h
+++ GameFrameWork/PP01.HelloSDL/Game.h
@@ -3,21 +3,21 @@
 
 class Game
 {
+	SDL_Texture* m_pTexture; // the new SDL_Texture variable
+	SDL_Rect m_sourceRectangle; // 원본 사각형 
+	SDL_Rect m_destinationRectangle; // 대상 사각형 
 public:
 	Game() {}
 	~Game() {}
-	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	bool init(const char* title, int xpos, int ypos,
+		int width, int height, bool fullscreen);
 	void render();
-	void update() {};
+	void update();
 	void handleEvents();
 	void clean();
 	bool running() { return m_bRunning; }
-
 private:
 	SDL_Window * m_pWindow;
-	SDL_Renderer*m_pRenderer;
-	SDL_Texture*m_pTexture;
-	SDL_Rect m_sourceRectangle;
-	SDL_Rect m_destinationRectangle;
+	SDL_Renderer* m_pRenderer;
 	bool m_bRunning;
 };
